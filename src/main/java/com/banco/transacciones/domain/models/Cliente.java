6d13f3ad59d6a3ce8a64c206dc16fd5763fd8bef
Roberto Alexandru Leahu <robertooleahu@gmail.com>
2026-04-08 10:29:31 +0200

    Añadir Clase Cliente.

---
+++ src/main/java/com/banco/transacciones/domain/models/Cliente.java
@@ -0,0 +1,41 @@
+package com.banco.transacciones.domain.models;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombre;
+    
+    @Column(unique = true)
+    private String dni;
+    
+    private String email;
+    
+    private LocalDate fechaAlta;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Cuenta> cuentas; // Relación 1:* 
+}
