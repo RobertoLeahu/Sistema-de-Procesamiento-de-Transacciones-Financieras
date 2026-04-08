2b8278026ab0e8f59ac5d120e6084d5738ab947d
Roberto Alexandru Leahu <robertooleahu@gmail.com>
2026-04-08 10:58:25 +0200

    Añadir Clase Cuenta.

---
+++ src/main/java/com/banco/transacciones/domain/models/Cuenta.java
@@ -0,0 +1,49 @@
+package com.banco.transacciones.domain.models;
+
+import java.math.BigDecimal;
+
+import com.banco.transacciones.domain.enums.EstadoCuenta;
+import com.banco.transacciones.domain.enums.TipoCuenta;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cuentas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cuenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true)
+    private String numeroCuenta;
+    
+    private BigDecimal saldo;
+    
+    @Enumerated(EnumType.STRING)
+    private TipoCuenta tipo;
+    
+    @Enumerated(EnumType.STRING)
+    private EstadoCuenta estado;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+}
