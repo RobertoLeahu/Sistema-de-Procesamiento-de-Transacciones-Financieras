3b10b1e90307cbb678201ff2cf18a53b5c20e071
Roberto Alexandru Leahu <robertooleahu@gmail.com>
2026-04-08 14:33:38 +0200

    Gestión de Errores y Logs: Añadir DTO de error (record).

---
+++ src/main/java/com/banco/transacciones/dto/response/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.banco.transacciones.dto.response;
+
+import java.time.Instant;
+
+/*
+ * Representa la estructura estándar de respuesta para cualquier error del sistema.
+ */
+
+public record ErrorResponse(
+		Instant timestamp,
+		int status,
+		String error,
+		String detalle,
+		String path
+		) {}
