d6b4622b1c626970307b45fac193fd1ff11e0c9e
Roberto Alexandru Leahu <robertooleahu@gmail.com>
2026-04-08 10:28:29 +0200

    Añadir Enum Nivel de Riesgo.

---
+++ src/main/java/com/banco/transacciones/domain/enums/NivelRiesgo.java
@@ -0,0 +1,12 @@
+package com.banco.transacciones.domain.enums;
+
+/*
+ * Representa los posibles niveles de riesgo de una transacción.
+ */
+
+public enum NivelRiesgo {
+	BAJO,
+	MEDIO,
+	ALTO,
+	CRITICO
+}
