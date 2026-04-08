1d341dc66c0eba83de6fe20d86da82ac4f05d22b
Roberto Alexandru Leahu <robertooleahu@gmail.com>
2026-04-08 10:27:15 +0200

    Añadir Enum TipoTransacción.

---
+++ src/main/java/com/banco/transacciones/domain/enums/TipoTransaccion.java
@@ -0,0 +1,13 @@
+package com.banco.transacciones.domain.enums;
+
+
+/*
+ * Representa los tipos posibles de una transacción.
+ * */
+
+public enum TipoTransaccion {
+	TRANSFERENCIA,
+	DEPOSITO,
+	RETIRO,
+	PAGO
+}
